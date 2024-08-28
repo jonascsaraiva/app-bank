36953fc0eb29f2817fd7bedf0be661adaa3accb3
jonascsaraiva <jonascsaraiva@outlook.com>
2024-08-27 22:57:27 -0300

    final commit

--- lib/services/transfer_service.dart
+++ lib/services/transfer_service.dart
@@ -1,8 +1,16 @@
 import 'package:dart_bank/models/account.dart';
 
-void transferService(Account origin, Account destination, double value) {
+void transferService(
+    Account origin, Account destination, double value, Account getbalance) {
   if (origin == destination) {
-    print("Esta tentando transfereir para mesma conta,transação negada.");
+    print("Transfereir para mesma conta não é permitido!");
+    return;
+  } else if (value <= 0) {
+    print("Conta sem saldo");
+    return;
+  } else if (value < getbalance.getBalance()) {
+    print("Saldo insuficiente");
+    return;
   } else {
     origin.removeBalance(value);
     destination.addBalance(value);
--- test/dart_bank_test.dart
+++ test/dart_bank_test.dart
@@ -8,10 +8,12 @@ void main() {
   group('transferService', () {
     late Account account1;
     late Account account2;
+    late Account account3;
 
     setUp(() {
       account1 = SimpleAccount('Diego', 1001, 1000.0);
       account2 = PremiumAccount('Arthur', 1002, 500.0, cashBack: 10.0);
+      account3 = SimpleAccount('Roberto', 1003, 10);
     });
 
     test('should transfer money from one simple account to premium account',
@@ -20,19 +22,25 @@ void main() {
 
       expect(account1.getBalance(), 800.0);
       expect(account2.getBalance(), 700.0);
+      print(account2);
+      return;
     });
 
-    test('should not transfer money from one simpleaccount to another', () {
-      transferService(account2, account2, 0);
-
-      expect(account2.getBalance(), 0);
+    test('should not transfer money from one account to another', () {
+      transferService(account3, account3, 20.0);
+      expect(account3.getBalance(), 0);
+      print(account3);
+      return;
     });
 
     test('should not transfer if account is in debth or zero', () {
-      transferService(account1, account2, 200.0);
+      transferService(account3, account1, 200.0);
 
-      expect(account2.getBalance(), 320);
-      expect(account2.getBalance(), 700.0);
+      expect(account3.getBalance(), 10.0);
+      expect(account1.getBalance(), 1000.0);
+
+      print(account1);
+      return;
     });
   });
 }
